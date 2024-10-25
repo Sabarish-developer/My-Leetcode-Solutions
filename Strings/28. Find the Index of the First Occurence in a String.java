308a6b5af6852d89f3a2fbdf9863724405869720
Sabarish-developer <sabarish.cspro@gmail.com>
2024-10-25 15:25:47 +0530

    Brute force

---
+++ Strings/28. Find the Index of the First Occurence in a String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        int index = haystack.indexOf(needle);
+        return index;
+
+    }
+}
+/*
+
+Time complexity - O(n * m)
+Space complexity - O(1)
+
+*/
