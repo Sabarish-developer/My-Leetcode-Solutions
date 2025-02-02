2865db9c662df6156dd0c2c07a4fb91dbe3d16dd
Sabarish-developer <sabarish.cspro@gmail.com>
2025-02-02 17:17:04 +0530

    Hashset

---
+++ LinkedList/3217. Delete Nodes From Linked List Present in Array.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        
+        Set<Integer> set = new HashSet<> ();
+        for(Integer i: nums)
+            set.add(i);
+        
+        while(head!=null && set.contains(head.val))
+            head = head.next;
+        
+        ListNode current = head;
+        while(current.next != null){
+            if(set.contains(current.next.val))
+                current.next = current.next.next;
+            else
+                current = current.next;
+        }
+        return head;
+    }
+}
+/*
+Time complexity - O(m + n)
+Space complexity - O(m)
+*/
