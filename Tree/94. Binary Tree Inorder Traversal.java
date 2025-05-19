200e3ef3301176ff7f34b9adc90772d27f915dd9
Sabarish-developer <sabarish.cspro@gmail.com>
2025-05-20 05:27:50 +0530

    morris inorder traversal

--- Tree/94. Binary Tree Inorder Traversal.java
+++ Tree/94. Binary Tree Inorder Traversal.java
@@ -1,34 +1,69 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> nodes = new ArrayList<>();
-        inorder(root, nodes);
+        
+        // Morris Inorder Traversal
+
+        List<Integer> nodes = new ArrayList<> ();
+        TreeNode current = root;
+        while(current != null){
+            if(current.left == null){
+                nodes.add(current.val);
+                current = current.right;
+            }
+            else{
+                TreeNode predecessor = current.left;
+                while(predecessor.right!=null && predecessor.right!=current){
+                    predecessor = predecessor.right;
+                }
+                if(predecessor.right == null){
+                    predecessor.right = current;
+                    current = current.left;
+                }
+                else{
+                    predecessor.right = null;
+                    nodes.add(current.val);
+                    current = current.right;
+                }
+            }
+        }
         return nodes;
     }
+}
 
-    private void inorder(TreeNode root, List<Integer> nodes) {
-        if (root == null)
-            return;
+/*
+Approach 1 : Recursive 
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> nodes = new ArrayList<>();
+        inorder(root,nodes);
+        return nodes;
+    }
+    private void inorder(TreeNode root, List<Integer> nodes){
+        if(root == null) return;
         inorder(root.left, nodes);
         nodes.add(root.val);
         inorder(root.right, nodes);
     }
 }
-/*
- * Time complexity - O(n)
- * Space complexity - O(n) for values + O(n) O(h) O(log n)
- */
+Time complexity - O(n)
+Space complexity - O(n) for values + O(n) O(h) O(log n)
+
+Approach 2 : Morris traversal
+Time complexity - O(n)
+Space complxity - O(1) Ignoring the space used to store values
+*/
